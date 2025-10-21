8f0deed7046d6847dbee0acc36cf45f1fcde85c1
jordanlao0309-svg <jordanlao0309@gmail.com>
2025-10-21 09:19:58 -0500

    subiendo cambias faltantes

---
+++ app/nosotros.tsx
@@ -0,0 +1,10 @@
+// app/nosotros.tsx
+import { Text, View } from 'react-native';
+
+export default function NosotrosScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Nosotros</Text>
+    </View>
+  );
+}
