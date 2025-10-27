ac19c561904262d8918499dde9b2049112440e16
CJRSantos <rengifosantoscj@gmail.com>
2025-10-27 11:53:50 -0500

    Installing a hammock

--- app/profile.tsx
+++ app/profile.tsx
@@ -16,7 +16,7 @@ import {
     View,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useTheme } from '../app/providers/ThemeProvider';
+import { useTheme } from '../app/providers/ThemeProvider'; 
 
 export default function ProfileScreen() {
     const router = useRouter();
